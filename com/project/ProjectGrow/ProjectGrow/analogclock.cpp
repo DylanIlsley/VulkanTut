9ed5809ed6554422a13d6be28f4d0434c78f3d74
Dylan <peterilsley97@gmail.com>
2021-04-29 21:52:51 +0200

    Additional items missed in previous commit
    As above

--- com/project/ProjectGrow/ProjectGrow/analogclock.cpp
+++ com/project/ProjectGrow/ProjectGrow/analogclock.cpp
@@ -46,19 +46,18 @@ void AnalogClock::paintEvent(QPaintEvent *event)
     // Drawing the hour hand
     painter.setPen(Qt::NoPen); // Don't want any outline so we use NoPen
     painter.setBrush(hourColour);
-
     painter.save();
-    painter.rotate(30.0 * ((time.hour() + time.minute()/60.0)));
+    painter.rotate(m_u16HourAngle * time.hour() + m_f16MinuteAngle * time.minute());
     painter.drawConvexPolygon(hourHand, 3);
     painter.restore();
 
     //Drawing hour edges around the clock
     painter.setPen(hourColour);
 
-    for (int i=0; i<12; ++i){
+    for (int i=0; i<24; ++i){
         painter.drawLine(88, 0 ,96, 0);
         // rotate to prepare for the next edge
-        painter.rotate(30.0);
+        painter.rotate(m_u16HourAngle);
     }
 
     //Draw the minute hand
@@ -66,25 +65,26 @@ void AnalogClock::paintEvent(QPaintEvent *event)
     painter.setBrush(minuteColour);
 
     painter.save();
-    painter.rotate(6.0 * (time.minute() + time.second()/60.0));
+    painter.rotate(6.0 * (time.minute() + time.second()/60.0)); // Not using the normal minute angle since it is 60 minutes around clock for drawing
     painter.drawConvexPolygon(minuteHand, 3);
     painter.restore();
 
     //Draw minute edges around the clock
-    painter.setPen(minuteColour);
+    // NOTE: Don't draw minute edges for military clock
+    /*painter.setPen(minuteColour);
     painter.save();
     for (int j=0; j<60; ++j){
         // Don't draw where the hour edge would be
         if((j%5) != 0 )
             painter.drawLine(92, 0, 96, 0);
-        painter.rotate(6.0); // get ready for next edge
+        painter.rotate(6.0); // Not using the normal minute angle since it is 60 minutes around clock for drawing
     }
-    painter.restore();
+    painter.restore();*/
 
     // Draw highlighted arc
     painter.setPen(Qt::NoPen);
     // Rotate to start at top
-    painter.rotate(-60);
+    //painter.rotate(-180 * 16);
     //Setting brush
     QColor BlueArc = QColor(0, 0, 120);
     BlueArc.setAlphaF(0.2);
@@ -92,8 +92,13 @@ void AnalogClock::paintEvent(QPaintEvent *event)
 
 
     QRectF rectangle(-100.0, -100.0, 200.0, 200.0);
-    int startAngle = -60 *16;
-    int spanAngle = -30 * 16;
+    // +90 to correct the angle to start on the y axis
+
+    int startAngle =(m_StartTime.hour()*m_u16HourAngle + m_StartTime.minute()*m_f16MinuteAngle)*16 - 90*16;
+    int spanAngle = ((m_StopTime.hour() - m_StartTime.hour())*m_u16HourAngle + (m_StopTime.minute() - m_StartTime.minute())*m_f16MinuteAngle) *16;
+    // Correction to make it go clockwise rather than counter clockwise
+    startAngle *= -1;
+    spanAngle  *=-1;
     painter.drawPie(rectangle, startAngle, spanAngle) ;
 }
 
